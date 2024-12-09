87ad62b2d4bcb510caedc94817d216aaf9c3f7d1
Paulo Amaral <53989502+phfa26@users.noreply.github.com>
2024-12-09 22:21:56 +1100

    initial client setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3007;
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
