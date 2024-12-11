78ba1a4f2fc50442516eeb6ee3c9b0cf067d4f48
Paulo Amaral <53989502+phfa26@users.noreply.github.com>
2024-12-12 00:49:13 +1100

    clean up

--- server/src/controllers/entryController.js
+++ server/src/controllers/entryController.js
@@ -9,7 +9,6 @@ exports.getAllEntries = (req, res) => {
 
 exports.addEntry = (req, res) => {
     const { question, response, date } = req.body;
-    console.log(question, response, date )
     db.run(`INSERT INTO entries (user_id, question, response, date) VALUES (?, ?, ?, ?)`, [req.user.id, question, response, date], function (err) {
         if (err) return res.status(500).json({ error: err.message });
         res.status(201).json({ id: this.lastID, question, response, date });
