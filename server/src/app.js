c7c9b0d62ab0a50b46dc54d277d0dccb577c3558
Paulo Amaral <53989502+phfa26@users.noreply.github.com>
2024-12-15 15:05:10 +1100

    typo

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { swaggerUi, swaggerDocs } = require('./swagger');
+const { swaggerUi, swaggerDocs } = require('../swagger');
 
 dotenv.config();
 
